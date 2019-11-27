2ae33ee9f759b57e1ce72df435ca4f37b2cb6f5c
Nelson Mbau <mbau.ndungu@gmail.com>
2019-11-28 01:42:43 +0300

    feat: Add storage loading

--- src/server.js
+++ src/server.js
@@ -1,9 +1,28 @@
 import express from "express"
+import storage from "./storage"
 
-const { PORT = 3000, NODE_ENV } = process.env
+import morgan from "morgan"
 
-const app = express()
+const { NODE_ENV, PORT = 3000 } = process.env;
+
+var app = express()
+
+if (NODE_ENV !== "test") app.use(morgan("tiny"))
+
+const attatchRouter = async () => {
+  const db = await storage
+
+  Object.assign(app.locals, { db })
+}
+
+attatchRouter()
 
 app.get("/", (req, res) => res.json({ version: "0.0.1" }))
+app.get("/:name", (req, res) => {
+  const { db } = req.app.locals
+  const { name } = req.params
+  console.log(db)
+  return res.json({ name })
+})
 
 app.listen(PORT, () => console.log(`Budgety started at ${PORT} on ${NODE_ENV} mode`))
